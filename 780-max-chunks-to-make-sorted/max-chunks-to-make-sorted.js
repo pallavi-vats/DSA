16050fc3c963e03da6e679d5e5ae7dcb3a6ee190
Pallavi kumari <144532693+pallavi-vats@users.noreply.github.com>
2025-07-27 10:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (6.78%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maxChunksToSorted = function(arr) {
+     let maxSoFar = 0, chunks = 0;
+    for (let i = 0; i < arr.length; i++) {
+        maxSoFar = Math.max(maxSoFar, arr[i]);
+        if (maxSoFar === i) {
+            chunks++;
+        }
+    }
+    return chunks;
+};
