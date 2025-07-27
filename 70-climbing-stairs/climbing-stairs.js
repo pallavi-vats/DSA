872b97238b8e61a8e8fa1cf32e79e1f3f920c772
Pallavi kumari <144532693+pallavi-vats@users.noreply.github.com>
2025-07-27 14:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (77.69%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n <= 2) return n;
+    let first = 1, second = 2;
+    for (let i = 3; i <= n; i++) {
+        let temp = first + second;
+        first = second;
+        second = temp;
+    }
+    return second;
+};
