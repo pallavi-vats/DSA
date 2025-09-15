4d88e57ab55f1f5da85ddcdc917de65178b8ec85
Pallavi kumari <144532693+pallavi-vats@users.noreply.github.com>
2025-09-15 20:49:12 +0530

    Time: 55 ms (33.64%) | Memory: 55.2 MB (62.22%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -5,15 +5,15 @@
 var reverse = function(x) {
     let rev=0
     let xcopy=x;
-    let n=Math.abs(x);
+    let n=Math.abs(x);// negative positive convertion
     while(n>0){
         let rem=n%10;
         rev=(10*rev)+rem;
         n=Math.floor(n/10);
     }
-    let limit=Math.pow(2,31);
+    let limit=Math.pow(2,31);//if outside this range than return 0
     if(rev<-limit||rev>limit)
     return 0;
-    return(xcopy<0)?-rev:rev;
+    return(xcopy<0)?-rev:rev;//ternery operator(if xcopy<0 than rturn negative number else positive )
     
 };
