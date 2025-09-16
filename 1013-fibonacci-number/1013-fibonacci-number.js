17bf64ab2f4c76ed7cf36c0af543b9e6e281c9f7
Pallavi kumari <144532693+pallavi-vats@users.noreply.github.com>
2025-09-16 20:21:40 +0530

    Time: 51 ms (57.59%), Space: 54.1 MB (22.12%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -8,12 +8,12 @@ var fib = function(n) {
   if(n==1) return 1;
   let a=0;
   let b=1;
-  
+  let c=a+b;
   for(let i=2;i<=n;i++){
-    let c=a+b;
+     c=a+b;
     a=b;
     b=c;
   }
-  return b;
+  return c;
 
 };
